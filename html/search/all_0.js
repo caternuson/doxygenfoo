1f2b670dd67e10b1948a839fbd9ce0ea955752b9
caternuson <caternuson@gmail.com>
2022-11-22 15:28:32 -0800

    remove html dir

--- html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['foo_0',['Foo',['../classFoo.html',1,'Foo'],['../classFoo.html#abddcaf1806543646a2757c81fdd98deb',1,'Foo::Foo(void)'],['../classFoo.html#a33891ee73e2ea35b8c3401cfb06be491',1,'Foo::Foo(int x, int y)']]]
-];
--- html/search/all_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['getx_1',['getX',['../classFoo.html#ae99925566547ed4e6cd58bb8d9e788b7',1,'Foo']]],
-  ['gety_2',['getY',['../classFoo.html#abd42b48859e6d73528d38d6e8548e038',1,'Foo']]]
-];
