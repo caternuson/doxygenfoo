b81b30134a83baa0e560a141d08e7074d6950d8e
Actions CI <actions_ci@nowhere.com>
2022-11-22 22:59:13 +0000

    add docs

---
+++ html/search/functions_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['getx_7',['getX',['../classFoo.html#ae99925566547ed4e6cd58bb8d9e788b7',1,'Foo']]],
+  ['gety_8',['getY',['../classFoo.html#abd42b48859e6d73528d38d6e8548e038',1,'Foo']]]
+];
